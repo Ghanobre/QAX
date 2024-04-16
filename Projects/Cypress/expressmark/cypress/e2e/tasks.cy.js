1e8073488f73f7b92c37e93b88d730e34b72962b
Ghanobre <152908735+Ghanobre@users.noreply.github.com>
2024-04-16 20:24:50 -0300

    commit

--- Projects/Cypress/expressmark/cypress/e2e/tasks.cy.js
+++ Projects/Cypress/expressmark/cypress/e2e/tasks.cy.js
@@ -1,6 +1,15 @@
 /// <reference types="cypress"/>
 
+import{faker} from '@faker-js/faker'
+
 describe('tarefas', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('#newTask')
+
+    it('deve cadastrar uma nova tarefa', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.get('input[placeholder="Add a new Task"]').type(faker.commerce.productName())
+
+        // cy.get('//button[contains(text(),"Create")]').click
+        cy.contains('button','Creat').click()
+    })
 })
