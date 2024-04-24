356e02964cc70cdd7afdf639ae424ed5c36d80ed
Ghanobre <152908735+Ghanobre@users.noreply.github.com>
2024-04-23 21:29:31 -0300

    commit

--- Projects/Python/main.py
+++ Projects/Python/main.py
@@ -1,34 +1,52 @@
-# import pandas as pd
+import pandas as pd
+import wget
+import os
 
-# pd.read_html('https://loterias.caixa.gov.br/Paginas/Lotofacil.aspx')
+def custom_progress_bar(current, total, width=80):
+    progress = current / total
+    bar_width = int(width * progress)
+    bar = '=' * bar_width + ' ' * (width - bar_width)
+    print(f'|{bar}| {progress:.0%}', end='\r')
 
-array0 = ('02','04','05','07','08','09','10','11','12','15','18','19','20','21','23')
-array1 = ('02','03','04','06','07','10','11','14','15','17','18','19','21','22','25')
-array2 = ('01','04','05','06','08','09','11','12','13','16','19','20','23','24','25')
-array3 = ('01','03','05','08','09','10','12','13','15','16','18','20','21','23','24')
-array4 = ('02','04','05','06','08','09','11','12','15','16','18','20','22','23','25')
+archive_file_path = "C:\QAX\Projects\Python\Fixtures\Lotofacil.xlsx"
+
+if os.path.exists(archive_file_path):
+    os.remove(archive_file_path)
+    print(f"{archive_file_path} has been deleted.")
+else:
+    print(f"{archive_file_path} does not exist.")
+
+link = "https://servicebus2.caixa.gov.br/portaldeloterias/api/resultados/download?modalidade=Lotof%C3%A1cil"
+wget.download (link,"C:\QAX\Projects\Python\Fixtures\Lotofacil.xlsx", bar= custom_progress_bar)
+print()
+
+resultanalise =list(pd.read_excel('C:\QAX\Projects\Python\Fixtures\Lotofacil.xlsx',skiprows=3076, usecols="C:Q"))
+
+array1 = (2,3,4,6,7,10,11,14,15,17,18,19,21,22,25)
+array2 = (1,4,5,6,8,9,11,12,13,16,19,20,23,24,25)
+array3 = (1,3,5,8,9,10,12,13,15,16,18,20,21,23,24)
+array4 = (2,4,5,6,8,9,11,12,15,16,18,20,22,23,25)
 
 posicoes_encontradas = 0
-
 for elemento in array1:
-    if elemento in array0:
+    if elemento in resultanalise:
         posicoes_encontradas +=1
-print(posicoes_encontradas)
+print('Jogo1 Gus',resultanalise,posicoes_encontradas)
 
 posicoes_encontradas = 0
 for elemento in array2:
-    if elemento in array0:
+    if elemento in resultanalise:
         posicoes_encontradas +=1
-print(posicoes_encontradas)
+print('Jogo2 Gus',resultanalise,posicoes_encontradas)
 
 posicoes_encontradas = 0
 for elemento in array3:
-    if elemento in array0:
+    if elemento in resultanalise:
         posicoes_encontradas +=1
-print(posicoes_encontradas)
+print('Jogo1 Ana',resultanalise,posicoes_encontradas)
 
 posicoes_encontradas = 0
 for elemento in array4:
-    if elemento in array0:
+    if elemento in resultanalise:
         posicoes_encontradas +=1
-print(posicoes_encontradas)
+print('Jogo2 Ana',resultanalise,posicoes_encontradas)
--- Projects/RobotFramework/walkdog/testes/home.robot
+++ Projects/RobotFramework/walkdog/testes/home.robot
@@ -5,7 +5,9 @@ Library    Browser
 
 *** Test Cases ***
 Home page deve estar online
-    New Browser    browser=chromium    headless=False
-    New Page     https://walkdog.vercel.app/
+    # New Browser    browser=chromium    headless=False
+    # New Page     https://walkdog.vercel.app/
     
-    Get Text    h1    equal    Cuidado e diversão em cada passo
+    # Get Text    h1    equal    Cuidado e diversão em cada passo
+
+    click    xpsth=
